3ac7c518c9e69102eadeee9199cc3bc95e218de3
Andy0414 <pjh8667@naver.com>
2020-08-08 00:57:54 +0900

    Update page

---
+++ src/views/Page.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="page"></div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+import TextInput from "@/components/TextInput.vue";
+import PasswordInput from "@/components/PasswordInput.vue";
+import Button from "@/components/Button.vue";
+
+@Component
+export default class Page extends Vue {}
+</script>
+
+<style lang="scss">
+.page {
+}
+</style>
