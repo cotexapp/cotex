009eb421a9306092030dfbd55244ce9a65336bf3
Andy0414 <pjh8667@naver.com>
2020-08-08 00:40:39 +0900

    Update store

--- src/store/index.ts
+++ src/store/index.ts
@@ -6,8 +6,43 @@ import axios from "axios";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-	state: {},
+	state: {
+		token: "",
+		userData: {},
+	},
 	mutations: {},
-	actions: {},
+	actions: {
+		async LOGIN({ commit, state }, data) {
+			try {
+				let res = (
+					await axios.post("/api/auth/user/login", {
+						userID: data.userID,
+						password: data.userID,
+					})
+				).data;
+				state.token = res.data;
+				let user = (
+					await axios.get("/api/auth/user/my", {
+						headers: {
+							Authorization: state.token,
+						},
+					})
+				).data.data;
+				state.userData = user;
+				return user;
+			} catch (err) {}
+		},
+		async REGISTER({ commit, state }, data) {
+			try {
+				let res = (
+					await axios.post("/api/auth/user", {
+						userID: data.userID,
+						password: data.userID,
+					})
+				).data;
+				return res.result;
+			} catch (err) {}
+		},
+	},
 	modules: {},
 });
